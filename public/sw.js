58547cc9b62f5f758e88f8057784090c51cfde59
DUUUINGDUCK <olanbad5@gmail.com>
2026-08-29 23:44:37 -0400

    Configure public/sw.js for Scramjet

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,1 @@
-importScripts("/scram/scramjet.all.js");
-const { ScramjetServiceWorker } = $scramjetLoadWorker();
-const scramjet = new ScramjetServiceWorker();
-self.addEventListener("fetch", event => event.respondWith((async () => {
-  await scramjet.loadConfig();
-  return scramjet.route(event) ? scramjet.fetch(event) : fetch(event.request);
-})()));
+importScripts("/scram/scramjet.all.js");const {ScramjetServiceWorker}=$scramjetLoadWorker();const scramjet=new ScramjetServiceWorker();self.addEventListener("fetch",event=>event.respondWith((async()=>{await scramjet.loadConfig();return scramjet.route(event)?scramjet.fetch(event):fetch(event.request)})()));
