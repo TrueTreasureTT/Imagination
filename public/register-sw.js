c00fdd88b7921e87aa97f3ecc37e30799ef35165
DUUUINGDUCK <olanbad5@gmail.com>
2026-08-29 23:44:39 -0400

    Configure public/register-sw.js for Scramjet

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,8 +1,1 @@
-const stockSW="/sw.js";
-async function registerSW(){
- if(!("serviceWorker" in navigator)) throw new Error("Service workers are not supported.");
- if(location.protocol!=="https:" && !["localhost","127.0.0.1"].includes(location.hostname)) throw new Error("HTTPS is required for service workers.");
- const registration=await navigator.serviceWorker.register(stockSW,{scope:"/"});
- await navigator.serviceWorker.ready;
- return registration;
-}
+const stockSW="/sw.js";async function registerSW(){if(!("serviceWorker"in navigator))throw new Error("Service workers are not supported.");if(location.protocol!=="https:"&&!["localhost","127.0.0.1"].includes(location.hostname))throw new Error("HTTPS is required for service workers.");const registration=await navigator.serviceWorker.register(stockSW,{scope:"/"});await navigator.serviceWorker.ready;return registration;}
